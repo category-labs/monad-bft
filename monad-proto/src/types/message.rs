c22612becd8f2f7648708f4f7a1d64cee7d169dc
Michael Chen <michaelchen0115@gmail.com>
2023-04-12 16:13:11 -0500

    rebasing onto refactored signing

--- monad-proto/src/types/message.rs
+++ monad-proto/src/types/message.rs
@@ -1,25 +1,16 @@
 use crate::error::ProtoError;
 use monad_consensus::types::message::VoteMessage;
-use monad_consensus::validation::signing::{Signed, Unverified};
+use monad_consensus::types::signature::ConsensusSignature;
+use monad_consensus::validation::signing::{Unverified, Verified};
 use prost::Message;
 
 include!(concat!(env!("OUT_DIR"), "/monad_proto.message.rs"));
 
 impl From<&VoteMessage> for ProtoVoteMessage {
-    fn from(votemsg: &VoteMessage) -> Self {
+    fn from(value: &VoteMessage) -> Self {
         ProtoVoteMessage {
-            vote_info: Some((&votemsg.vote_info).into()),
-            ledger_commit_info: Some((&votemsg.ledger_commit_info).into()),
-        }
-    }
-}
-
-impl From<&Unverified<VoteMessage>> for ProtoUnverifiedVoteMessage {
-    fn from(votemsg: &Unverified<VoteMessage>) -> Self {
-        ProtoUnverifiedVoteMessage {
-            vote_msg: Some((&votemsg.0.obj).into()),
-            author: Some((&votemsg.0.author).into()),
-            author_signature: Some((&votemsg.0.author_signature).into()),
+            vote_info: Some((&value.vote_info).into()),
+            ledger_commit_info: Some((&value.ledger_commit_info).into()),
         }
     }
 }
@@ -27,15 +18,15 @@ impl From<&Unverified<VoteMessage>> for ProtoUnverifiedVoteMessage {
 impl TryFrom<ProtoVoteMessage> for VoteMessage {
     type Error = ProtoError;
 
-    fn try_from(proto_votemsg: ProtoVoteMessage) -> Result<Self, Self::Error> {
+    fn try_from(value: ProtoVoteMessage) -> Result<Self, Self::Error> {
         Ok(Self {
-            vote_info: proto_votemsg
+            vote_info: value
                 .vote_info
                 .ok_or(Self::Error::MissingRequiredField(
                     "VoteMessage.vote_info".to_owned(),
                 ))?
                 .try_into()?,
-            ledger_commit_info: proto_votemsg
+            ledger_commit_info: value
                 .ledger_commit_info
                 .ok_or(Self::Error::MissingRequiredField(
                     "VoteMessage.ledger_commit_info".to_owned(),
@@ -45,34 +36,35 @@ impl TryFrom<ProtoVoteMessage> for VoteMessage {
     }
 }
 
+impl From<&Verified<VoteMessage>> for ProtoUnverifiedVoteMessage {
+    fn from(value: &Verified<VoteMessage>) -> Self {
+        Self {
+            vote_msg: Some((&(**value)).into()),
+            author_signature: Some(value.author_signature().into()),
+        }
+    }
+}
+
 impl TryFrom<ProtoUnverifiedVoteMessage> for Unverified<VoteMessage> {
     type Error = ProtoError;
     fn try_from(value: ProtoUnverifiedVoteMessage) -> Result<Self, Self::Error> {
-        Ok(Unverified::<VoteMessage> {
-            0: Signed::<VoteMessage, false> {
-                obj: value
-                    .vote_msg
-                    .ok_or(Self::Error::MissingRequiredField(
-                        "Unverified<VoteMessage>.obj".to_owned(),
-                    ))?
-                    .try_into()?,
-                author: value
-                    .author
-                    .ok_or(Self::Error::MissingRequiredField(
-                        "Unverified<VoteMessage>.author".to_owned(),
-                    ))?
-                    .try_into()?,
-                author_signature: value
-                    .author_signature
-                    .ok_or(Self::Error::MissingRequiredField(
-                        "Unverified<VoteMessage>.signature".to_owned(),
-                    ))?
-                    .try_into()?,
-            },
-        })
+        let msg: VoteMessage = value
+            .vote_msg
+            .ok_or(Self::Error::MissingRequiredField(
+                "Unverified<VoteMessage>.obj".to_owned(),
+            ))?
+            .try_into()?;
+        let signature: ConsensusSignature = value
+            .author_signature
+            .ok_or(Self::Error::MissingRequiredField(
+                "Unverified<VoteMessage>.signature".to_owned(),
+            ))?
+            .try_into()?;
+        Ok(Unverified::new(msg, signature))
     }
 }
-pub fn serialize_unverified_vote_message(votemsg: &Unverified<VoteMessage>) -> Vec<u8> {
+
+pub fn serialize_verified_vote_message(votemsg: &Verified<VoteMessage>) -> Vec<u8> {
     let proto_votemsg: ProtoUnverifiedVoteMessage = votemsg.into();
     let mut buf = Vec::with_capacity(proto_votemsg.encoded_len());
     proto_votemsg.encode(&mut buf).unwrap();
--- monad-proto/tests/protobuf.rs
+++ monad-proto/tests/protobuf.rs
@@ -7,17 +7,18 @@ mod test {
         },
         validation::{
             hashing::{Hasher, Sha256Hash},
-            signing::Signable,
+            signing::{Unverified, ValidatorMember},
         },
     };
     use monad_crypto::secp256k1::KeyPair;
     use monad_proto::types::message::{
-        deserialize_unverified_vote_message, serialize_unverified_vote_message,
+        deserialize_unverified_vote_message, serialize_verified_vote_message,
     };
     use monad_types::{BlockId, NodeId, Round};
+    use monad_validator::validator::Validator;
 
     #[test]
-    fn test_unverified_vote_message() {
+    fn test_verified_vote_message() {
         let vi = VoteInfo {
             id: BlockId(vec![42; 32].try_into().unwrap()),
             round: Round(1),
@@ -38,13 +39,25 @@ mod test {
                 .unwrap();
         let keypair = KeyPair::from_slice(&privkey).unwrap();
         let author = NodeId(keypair.pubkey());
+        let mut validators = ValidatorMember::new();
+        validators.insert(
+            author,
+            Validator {
+                pubkey: keypair.pubkey(),
+                stake: 1,
+            },
+        );
 
         let hash = Sha256Hash::hash_object(&votemsg.ledger_commit_info);
         let sig = ConsensusSignature(keypair.sign(&hash));
 
-        let signed_votemsg = votemsg.signed_object(author, sig);
+        let signed_votemsg = Unverified::new(votemsg, sig);
+        let verified_votemsg = signed_votemsg
+            .clone()
+            .verify::<Sha256Hash>(&validators, &author.0)
+            .unwrap();
 
-        let buf = serialize_unverified_vote_message(&signed_votemsg);
+        let buf = serialize_verified_vote_message(&verified_votemsg);
         let de_votemsg = deserialize_unverified_vote_message(buf.as_ref());
 
         assert_eq!(signed_votemsg, de_votemsg.unwrap());
