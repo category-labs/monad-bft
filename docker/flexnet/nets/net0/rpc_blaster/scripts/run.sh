30a50266a0a95ce11d5a6e499ff9c82417c0cadf
michaelchen0115 <michaelchen0115@gmail.com>
2024-02-29 16:42:45 -0500

    Flexnet changes after removing hash from ledger blocks

--- docker/flexnet/nets/net0/rpc_blaster/scripts/run.sh
+++ docker/flexnet/nets/net0/rpc_blaster/scripts/run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-groupadd -g $HOST_GID user
-useradd -u $HOST_UID -g user -N user
+mkdir /monad/logs
 
-su -c "mkdir /monad/logs" -m user 
-
-su -c "python3 /monad/scripts/blaster.py --rpc http://rpc0:8080 --data /data/txns.json > /monad/logs/rpc-blaster.log 2>&1" -m user
+# ping -c 10 rpc0 > /monad/logs/ping.log 2>&1
+# curl -vv --header "Content-Type: application/json" --request POST --data '{"jsonrpc": "2.0", "method": "eth_sendRawTransaction", "params": [], "id": 0}' http://rpc0:8080 > /monad/logs/curl.log 2>&1
+python3 /monad/scripts/blaster.py --rpc http://rpc0:8080 --data /data/txns.json > /monad/logs/rpc-blaster.log 2>&1
