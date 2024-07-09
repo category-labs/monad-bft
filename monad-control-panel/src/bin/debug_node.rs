26c9711c1f5c275b49428ff4386778f8397bccad
Rene Garcia <rgarc@alum.mit.edu>
2024-07-09 11:05:04 -0400

    Add debug node binary

---
+++ monad-control-panel/src/bin/debug_node.rs
@@ -0,0 +1,161 @@
+use std::{io::Error, path::PathBuf};
+
+use clap::{Parser, Subcommand};
+use futures::{SinkExt, StreamExt};
+use monad_bls::BlsSignatureCollection;
+use monad_consensus_types::{
+    signature_collection::SignatureCollection, validator_data::ParsedValidatorData,
+};
+use monad_crypto::certificate_signature::CertificateSignaturePubKey;
+use monad_executor_glue::{
+    ClearMetrics, ControlPanelCommand, GetValidatorSet, ReadCommand, UpdateValidatorSet,
+    WriteCommand,
+};
+use monad_secp::SecpSignature;
+use tokio::net::{
+    unix::{OwnedReadHalf, OwnedWriteHalf},
+    UnixStream,
+};
+use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
+
+type SignatureType = SecpSignature;
+type SignatureCollectionType = BlsSignatureCollection<CertificateSignaturePubKey<SignatureType>>;
+type Command = ControlPanelCommand<SignatureCollectionType>;
+
+/// CLI program to manage validators and metrics.
+#[derive(Parser, Debug)]
+#[command(name = "Validator Manager")]
+#[command(about = "CLI program to manage validators and metrics", long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    control_panel_ipc_path: PathBuf,
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Displays the list of validators
+    Validators,
+    /// Updates the validators using the provided TOML file
+    UpdateValidators {
+        /// Path to the TOML file
+        #[arg(short, long, value_name = "FILE")]
+        path: PathBuf,
+    },
+    /// Clears the metrics
+    ClearMetrics,
+}
+
+struct Read {
+    read: FramedRead<OwnedReadHalf, LengthDelimitedCodec>,
+}
+
+impl Read {
+    pub async fn next<SCT: SignatureCollection>(
+        &mut self,
+    ) -> Result<ControlPanelCommand<SCT>, Error> {
+        let bytes = self
+            .read
+            .next()
+            .await
+            .ok_or(Error::other("client stream ended"))??;
+        bincode::deserialize(&bytes).map_err(Error::other)
+    }
+}
+
+struct Write {
+    write: FramedWrite<OwnedWriteHalf, LengthDelimitedCodec>,
+}
+
+impl Write {
+    pub async fn send<SCT: SignatureCollection>(
+        &mut self,
+        request: ControlPanelCommand<SCT>,
+    ) -> Result<(), Error> {
+        let bytes = bincode::serialize(&request).map_err(Error::other)?;
+        self.write.send(bytes.into()).await
+    }
+}
+
+fn main() -> Result<(), Error> {
+    let cli = Cli::parse();
+    let socket_path = cli.control_panel_ipc_path;
+
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    let (read, write) = rt.block_on(async move {
+        let client_stream = UnixStream::connect(socket_path.as_path()).await?;
+
+        let (read, write) = client_stream.into_split();
+
+        let read = FramedRead::new(read, LengthDelimitedCodec::default());
+        let write = FramedWrite::new(write, LengthDelimitedCodec::default());
+
+        Ok::<_, Error>((read, write))
+    })?;
+
+    let mut read = Read { read };
+    let mut write = Write { write };
+
+    match cli.command {
+        Commands::Validators => {
+            rt.block_on(write.send(Command::Read(ReadCommand::GetValidatorSet(
+                GetValidatorSet::Request,
+            ))))?;
+
+            let response = rt.block_on(read.next::<SignatureCollectionType>())?;
+
+            let parsed_validator_set = match response {
+                ControlPanelCommand::Read(read) => match read {
+                    ReadCommand::GetValidatorSet(v) => match v {
+                        GetValidatorSet::Response(s) => s,
+                        r => {
+                            return Err(Error::other(format!(
+                                "expected validator set response, got {:?}",
+                                r
+                            )));
+                        }
+                    },
+                },
+                r => {
+                    return Err(Error::other(format!(
+                        "expected validator set response, got {:?}",
+                        r
+                    )));
+                }
+            };
+            println!("{}", toml::to_string(&parsed_validator_set).unwrap());
+        }
+        Commands::UpdateValidators { path } => {
+            let toml_choice = path;
+
+            let update_validator_set =
+                toml::from_str::<ParsedValidatorData<SignatureCollectionType>>(
+                    &std::fs::read_to_string(toml_choice).unwrap(),
+                )
+                .map_err(Error::other)?;
+            let request = Command::Write(WriteCommand::UpdateValidatorSet(
+                UpdateValidatorSet::Request(update_validator_set),
+            ));
+
+            rt.block_on(write.send(request))?;
+
+            rt.block_on(write.send(Command::Read(ReadCommand::GetValidatorSet(
+                GetValidatorSet::Request,
+            ))))?;
+
+            let response = rt.block_on(read.next::<SignatureCollectionType>())?;
+            println!("{}", serde_json::to_string(&response).unwrap());
+        }
+        Commands::ClearMetrics => {
+            rt.block_on(write.send(Command::Write(WriteCommand::ClearMetrics(
+                ClearMetrics::Request,
+            ))))?;
+
+            let response = rt.block_on(read.next::<SignatureCollectionType>())?;
+            println!("{}", serde_json::to_string(&response).unwrap());
+        }
+    }
+
+    Ok(())
+}
--- monad-control-panel/src/lib.rs
+++ monad-control-panel/src/lib.rs
@@ -1,4 +1,30 @@
 pub mod ipc;
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use monad_bls::BlsSignatureCollection;
+    use monad_consensus_types::validator_data::ParsedValidatorData;
+    use monad_crypto::certificate_signature::CertificateSignaturePubKey;
+    use monad_secp::SecpSignature;
+
+    type SignatureType = SecpSignature;
+    type SignatureCollectionType =
+        BlsSignatureCollection<CertificateSignaturePubKey<SignatureType>>;
+
+    #[test]
+    fn test_json() {
+        let input = r#"
+epoch = 10
+[[validators]]
+node_id = "0x02093852f554d6196025b0bebe21ea646f73e34f1fd6f154e6ae77c890363ea5be"
+stake = 1
+cert_pubkey = "0xab1011a17be0921c122fc7362f6e0401f80623c644bb9d9e9eedff0410a4de2259f0d3a36414d4dd9dd748ace56ffb5e"
+        "#;
+        let parsed_validators: ParsedValidatorData<SignatureCollectionType> =
+            toml::from_str(input).unwrap();
+        println!(
+            "{}",
+            serde_json::to_string_pretty(&parsed_validators).unwrap()
+        );
+    }
+}
