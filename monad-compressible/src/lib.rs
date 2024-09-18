be3c3a9017a233fd2fadf1d794cc4e6a91c470eb
Rene Garcia <rgarc@alum.mit.edu>
2024-09-18 12:42:11 -0400

    wip

--- monad-compressible/src/lib.rs
+++ monad-compressible/src/lib.rs
@@ -1,23 +1,24 @@
 use monad_compress::lz4::Lz4Compression;
-use monad_consensus_types::signature_collection::SignatureCollection;
+use monad_consensus::messages::{consensus_message::ProtocolMessage, message::ProposalMessage};
+use monad_consensus::validation::signing::Validated;
+use monad_consensus_types::{
+    payload::TransactionPayload, signature_collection::SignatureCollection,
+};
 use monad_crypto::certificate_signature::{
     CertificateSignaturePubKey, CertificateSignatureRecoverable,
 };
+use monad_state::VerifiedMonadMessage;
+
 pub trait Compressible {
     type Output;
-    fn compress(&self) -> Self::Output;
+    type Error;
+    fn compress(&self) -> Result<Self::Output, Self::Error>;
 }
 
-impl<ST, SCT> Compressible for monad_state::MonadMessage<ST, SCT>
-where
-    ST: CertificateSignatureRecoverable,
-    SCT: SignatureCollection<NodeIdPubKey = CertificateSignaturePubKey<ST>>,
-{
-    type Output = monad_state::MonadMessage<ST, SCT>;
-
-    fn compress(&self) -> Self::Output {
-        todo!()
-    }
+#[derive(Debug)]
+pub enum CompressionError {
+    Unsupported,
+    AlreadyCompressed,
 }
 
 impl<ST, SCT> Compressible for monad_state::VerifiedMonadMessage<ST, SCT>
@@ -26,9 +27,23 @@ where
     SCT: SignatureCollection<NodeIdPubKey = CertificateSignaturePubKey<ST>>,
 {
     type Output = monad_state::VerifiedMonadMessage<ST, SCT>;
+    type Error = CompressionError;
 
-    fn compress(&self) -> Self::Output {
-        let compress = Lz4Compression::new(0);
-        todo!()
+    fn compress(&self) -> Result<Self::Output, Self::Error> {
+        match self.clone() {
+            VerifiedMonadMessage::Consensus(m) => match &m.message {
+                ProtocolMessage::Proposal(ProposalMessage { payload, .. }) => match &payload.txns {
+                    TransactionPayload::List(full_transaction_list) => {
+                        Ok(VerifiedMonadMessage::Consensus())
+                    }
+                    TransactionPayload::CompressedList { .. } => {
+                        Err(CompressionError::AlreadyCompressed)
+                    }
+                    TransactionPayload::Null => Err(CompressionError::Unsupported),
+                },
+                _ => Err(CompressionError::Unsupported),
+            },
+            _ => Err(CompressionError::Unsupported),
+        }
     }
 }
