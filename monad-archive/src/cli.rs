4824eef8b7ce948d861e9e1087c19367422e7bb4
Tong Zhi <tong@monad.xyz>
2024-10-31 17:32:31 -0400

    Archive setup
    Add table schema
    
    block number padding
    
    Restart from latest block number
    
    Fix format errors
    
    Modify types
    
    Add reader method interfaces
    
    implement get_block_by_number endpoint
    
    Add block hash indexing table
    
    Minor format
    
    Finish all block readers
    
    Change type to ReceiptWithBloom
    
    Archive receipts too; sync primitives
    
    Format
    
    Use real monad block data
    
    Add transaction table
    
    Fix format errors
    
    Add tx reader methods
    
    Add receipt writers
    
    Add receipt readers
    
    timing code
    
    async block processing
    
    Parallelize receipt & tx upload
    
    Add receipt reader function
    
    Reorganize interface
    
    Implement retry
    
    Use async for spawn; Semaphore for control; Compress tables
    
    Remove reader methods
    
    Optimization1
    
    Remove hash uploads
    
    Address feedback #2

---
+++ monad-archive/src/cli.rs
@@ -0,0 +1,24 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(name = "monad-archive", about, long_about = None)]
+pub struct Cli {
+    #[arg(long)]
+    pub triedb_path: PathBuf,
+
+    #[arg(long)]
+    pub s3_bucket: String,
+
+    #[arg(long)]
+    pub s3_region: Option<String>,
+
+    /// Set the max response size in bytes (the same as monad-rpc)
+    #[arg(long, default_value_t = 25_000_000)]
+    pub max_response_size: u32,
+
+    /// Set the concurrent blocks
+    #[arg(long, default_value_t = 10)]
+    pub max_concurrent_blocks: usize,
+}
---
+++ monad-archive/src/main.rs
@@ -0,0 +1,152 @@
+use std::{sync::Arc, time::Instant};
+
+use clap::Parser;
+use futures::future::join_all;
+use reth_primitives::ReceiptWithBloom;
+use s3_archive::S3ArchiveWriter;
+use tokio::{
+    sync::Semaphore,
+    time::{sleep, Duration},
+    try_join,
+};
+use tracing::{error, info, Level};
+use triedb::Triedb;
+
+use crate::{
+    archive_interface::ArchiveWriterInterface, cli::Cli, errors::ArchiveError,
+    s3_archive::S3Archive, triedb::TriedbEnv,
+};
+
+mod archive_interface;
+mod cli;
+mod errors;
+mod s3_archive;
+mod triedb;
+
+#[tokio::main]
+async fn main() -> Result<(), ArchiveError> {
+    tracing_subscriber::fmt().with_max_level(Level::INFO).init();
+
+    let args = Cli::parse();
+
+    let max_concurrent_blocks = args.max_concurrent_blocks;
+    let concurrent_block_semaphore = Arc::new(Semaphore::new(max_concurrent_blocks));
+
+    // This will spin off a polling thread
+    let triedb = TriedbEnv::new(&args.triedb_path);
+
+    // Construct an s3 instance
+    let s3_archive = S3Archive::new(args.s3_bucket, args.s3_region).await?;
+    let s3_archive_writer = S3ArchiveWriter::new(s3_archive).await?;
+
+    let mut latest_processed_block = (s3_archive_writer.get_latest().await).unwrap_or_default();
+
+    info!("Latest processed block is : {}", latest_processed_block);
+
+    // Check for new blocks every 10 ms
+    // Issue requests to triedb, poll data and push to relevant tables
+    loop {
+        sleep(Duration::from_millis(10)).await;
+
+        let block_number = match triedb.get_latest_block().await {
+            Ok(number) => number,
+            Err(e) => return Err(e),
+        };
+
+        if block_number <= latest_processed_block {
+            continue;
+        }
+
+        let start = Instant::now();
+        info!(
+            "Processing blocks from {} to {}, start time = {:?}",
+            latest_processed_block + 1,
+            block_number,
+            start
+        );
+
+        let start_block_number = if latest_processed_block == 0 {
+            0
+        } else {
+            latest_processed_block + 1
+        };
+
+        let join_handles = (start_block_number..=block_number).map(|current_block: u64| {
+            let triedb = triedb.clone();
+            let s3_archive_writer = s3_archive_writer.clone();
+
+            let semaphore = concurrent_block_semaphore.clone();
+            tokio::spawn(async move {
+                let permit = semaphore
+                    .acquire()
+                    .await
+                    .expect("Got permit to execute a new block");
+                let _ = handle_block(&triedb, current_block, &s3_archive_writer).await;
+                std::mem::drop(permit);
+            })
+        });
+
+        let block_results = join_all(join_handles).await;
+        let mut current_join_block = start_block_number;
+        for block_result in block_results {
+            match block_result {
+                Ok(_) => {
+                    current_join_block += 1;
+                }
+                Err(e) => {
+                    if current_join_block != 0 {
+                        s3_archive_writer
+                            .update_latest(current_join_block - 1)
+                            .await?;
+                    }
+                    return Err(ArchiveError::custom_error(format!(
+                        "Failure in writing block {}, {:?}",
+                        current_join_block, e
+                    )));
+                }
+            }
+        }
+
+        s3_archive_writer.update_latest(block_number).await?;
+        latest_processed_block = block_number;
+
+        let duration = start.elapsed();
+        info!("Time spent = {:?}", duration);
+    }
+}
+
+async fn handle_block(
+    triedb: &TriedbEnv,
+    current_block: u64,
+    s3_archive: &S3ArchiveWriter,
+) -> Result<(), ArchiveError> {
+    /*  Store Blocks */
+    let block_header = match triedb.get_block_header(current_block).await? {
+        Some(header) => header,
+        None => return Ok(()),
+    };
+    let transactions = triedb.get_transactions(current_block).await?;
+
+    let f_block = s3_archive.archive_block(block_header, transactions, current_block);
+
+    /* Store Receipts */
+    let receipts: Vec<ReceiptWithBloom> = triedb.get_receipts(current_block).await?;
+
+    let f_receipt = s3_archive.archive_receipts(receipts, current_block);
+
+    /* Store Traces */
+    let traces: Vec<Vec<u8>> = triedb.get_call_frames(current_block).await?;
+
+    let f_trace = s3_archive.archive_traces(traces, current_block);
+
+    match try_join!(f_block, f_receipt, f_trace) {
+        Ok(_) => {
+            info!("Successfully archived block {}", current_block);
+        }
+        Err(e) => {
+            error!("Error archiving block {}: {:?}", current_block, e);
+        }
+    }
+
+    Ok(())
+}
