547e5f90cbd64aa6982c32149eb926e6cfee6ec4
Vicky Chen <vchen@category.xyz>
2025-09-01 00:31:35 -0400

    Bump execution to switch to a memory-bounded cache for TrieDB nodes and expose memory limit in cli args

--- monad-triedb/src/lib.rs
+++ monad-triedb/src/lib.rs
@@ -156,13 +156,15 @@ pub unsafe extern "C" fn traverse_callback(
 }
 
 impl TriedbHandle {
-    pub fn try_new(dbdir_path: &Path) -> Option<Self> {
+    pub fn try_new(dbdir_path: &Path, node_lru_max_mem: u64) -> Option<Self> {
         let path = CString::new(dbdir_path.to_str().expect("invalid path"))
             .expect("failed to create CString");
 
         let mut db_ptr = null_mut();
 
-        let result = unsafe { bindings::triedb_open(path.as_c_str().as_ptr(), &mut db_ptr) };
+        let result = unsafe {
+            bindings::triedb_open(path.as_c_str().as_ptr(), &mut db_ptr, node_lru_max_mem)
+        };
 
         if result != 0 {
             debug!("triedb try_new error result: {}", result);
