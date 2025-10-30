94574cf828573ba6d24f4d4d2e31567b6004df3d
rtsaimonad <rtsai@category.xyz>
2025-10-30 15:59:11 -0700

    FORK_ROUND, FORK_EPOCH_STAKING, FORK_EPOCH_STAKING_REWARDS, FORK_UNIX_TS_S

--- monad-chain-config/src/lib.rs
+++ monad-chain-config/src/lib.rs
@@ -167,14 +167,14 @@ const MONAD_DEVNET_CHAIN_CONFIG: MonadChainConfig = MonadChainConfig {
     v_0_7_0_activation: Round::MIN,
     v_0_8_0_activation: Round::MIN,
     v_0_10_0_activation: Round::MIN,
-    v_0_11_0_activation: Round(FORK_ROUND),
+    v_0_11_0_activation: Round(14500),
 
-    staking_activation: Epoch(FORK_EPOCH_STAKING),
-    staking_rewards_activation: Epoch(FORK_EPOCH_STAKING_REWARDS),
+    staking_activation: Epoch(4),
+    staking_rewards_activation: Epoch(4_REWARDS),
 
     execution_v_one_activation: 0,
     execution_v_two_activation: 0,
-    execution_v_four_activation: FORK_UNIX_TS_S,
+    execution_v_four_activation: 1761884842,
 };
 
 const MONAD_TESTNET_CHAIN_CONFIG: MonadChainConfig = MonadChainConfig {
