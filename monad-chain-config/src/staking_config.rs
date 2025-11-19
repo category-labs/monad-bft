d258821921fea80bd6bc5aa0674bf852c10a3d02
rtsaimonad <rtsai@category.xyz>
2025-11-18 16:50:08 -0800

    donet testing

--- monad-chain-config/src/staking_config.rs
+++ monad-chain-config/src/staking_config.rs
@@ -59,8 +59,12 @@ pub const MONAD_DEVNET_STAKING_CONFIG: MonadStakingConfig = MonadStakingConfig {
     staking_activation: Epoch(2),
 
     block_reward_v_one: BlockRewardConfig {
-        block_reward_activation: Epoch(3),
+        block_reward_activation: Epoch(5),
         block_reward_mon: 1,
+    },
+    block_reward_v_two: BlockRewardConfig {
+        block_reward_activation: Epoch(7),
+        block_reward_mon: 25,
     },
     block_reward_v_two: BlockRewardConfig::unused(),
 };
