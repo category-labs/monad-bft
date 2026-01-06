d3b3b30421dec86d290202f3ba124f35022c0c4f
michael-yxchen <michaelchen0115@gmail.com>
2026-01-06 17:43:19 -0500

    ConfirmGroup message fix
    It was possible to create a ConfirmGroup message with peer list longer
    than max_group_size in the original invitation. This ConfirmGroup
    message will get rejected by full nodes. This PR introduces a NoConfirm
    message that's immediately sent to the client when group reaches max
    size. When client receives the NoConfirm message, it releases the round
    to other invites.
    
    Regarding backwards compatibility, the NoConfirm message is a new
    addition. Older clients will simply ignore this message (fail to
    deserialize) and will not experience any negative effects.

--- monad-raptorcast/src/lib.rs
+++ monad-raptorcast/src/lib.rs
@@ -1370,6 +1370,7 @@ where
         }
         FullNodesGroupMessage::PrepareGroupResponse(msg) => &msg.node_id == sender,
         FullNodesGroupMessage::ConfirmGroup(msg) => &msg.prepare.validator_id == sender,
+        FullNodesGroupMessage::NoConfirm(msg) => &msg.prepare.validator_id == sender,
     }
 }
 
