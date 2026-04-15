e9695c2df157212f814fafc4a2a0a9981842f715
Andre Benedito <38482987+andr-dev@users.noreply.github.com>
2026-04-15 17:06:52 -0400

    Introduce datasource module in RPC

---
+++ monad-rpc/src/data/source/stack.rs
@@ -0,0 +1,32 @@
+// Copyright (C) 2025 Category Labs, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use async_trait::async_trait;
+
+use super::HistoricalDataSource;
+
+#[derive(Clone)]
+pub struct DataSourceStack<T> {
+    sources: Box<[T]>,
+}
+
+impl<T> DataSourceStack<T> {
+    pub fn new(sources: Box<[T]>) -> Self {
+        Self { sources }
+    }
+}
+
+#[async_trait]
+impl<T> HistoricalDataSource for DataSourceStack<T> where T: HistoricalDataSource + Clone {}
