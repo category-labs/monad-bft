4421c9d4970b215140a10e46104c22bf6f6bf9a5
Andre Benedito <38482987+andr-dev@users.noreply.github.com>
2026-05-11 14:49:40 -0400

    Add block retrieval methods to HistoricalDataSource

--- monad-rpc/src/data/source/archive.rs
+++ monad-rpc/src/data/source/archive.rs
@@ -13,10 +13,18 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+use alloy_consensus::Header;
+use alloy_primitives::BlockHash;
 use async_trait::async_trait;
-use monad_archive::prelude::ArchiveReader;
+use monad_archive::{
+    model::BlockDataReader,
+    prelude::{ArchiveReader, LatestKind},
+};
+use monad_eth_types::TxEnvelopeWithSender;
 
-use super::HistoricalDataSource;
+use super::{
+    BlockCommitState, BlockPointer, DataSourceError, DataSourceResult, HistoricalDataSource,
+};
 
 #[derive(Clone)]
 pub struct ArchiveDataSource {
@@ -29,5 +37,82 @@ impl ArchiveDataSource {
     }
 }
 
+async fn get_block_from_archive(
+    archive_reader: &ArchiveReader,
+    pointer: BlockPointer,
+) -> DataSourceResult<Option<alloy_consensus::Block<TxEnvelopeWithSender>>> {
+    match pointer {
+        // Archive cannot serve non-finalized blocks.
+        BlockPointer::NonFinalized(..) => Ok(None),
+
+        BlockPointer::Finalized(block_num) => {
+            archive_reader.try_get_block_by_number(block_num).await
+        }
+    }
+    .map_err(|e| DataSourceError::Internal(e.to_string()))
+}
+
 #[async_trait]
-impl HistoricalDataSource for ArchiveDataSource {}
+impl HistoricalDataSource for ArchiveDataSource {
+    async fn try_resolve_block_commit_state(
+        &self,
+        commit_state: BlockCommitState,
+    ) -> DataSourceResult<Option<BlockPointer>> {
+        match commit_state {
+            BlockCommitState::Proposed | BlockCommitState::Voted => Ok(None),
+            BlockCommitState::Finalized => {
+                let Some(latest_block_num) = self
+                    .archive_reader
+                    .get_latest(LatestKind::Uploaded)
+                    .await
+                    .map_err(|e| DataSourceError::Internal(e.to_string()))?
+                else {
+                    return Ok(None);
+                };
+
+                Ok(Some(BlockPointer::Finalized(latest_block_num)))
+            }
+        }
+    }
+
+    async fn try_resolve_block_number(
+        &self,
+        block_number: u64,
+    ) -> DataSourceResult<Option<BlockPointer>> {
+        // TODO(andr-dev): Archive currently assumes all block numbers are finalized, this needs to
+        // be changed to enable archive before other sources that can serve non-finalized,
+        // deferring to keep things simple for now.
+        Ok(Some(BlockPointer::Finalized(block_number)))
+    }
+
+    async fn try_resolve_block_hash(
+        &self,
+        block_hash: BlockHash,
+    ) -> DataSourceResult<Option<BlockPointer>> {
+        let Some(block_number) = self
+            .archive_reader
+            .try_get_block_number_by_hash(&block_hash)
+            .await
+            .map_err(|e| DataSourceError::Internal(e.to_string()))?
+        else {
+            return Ok(None);
+        };
+
+        Ok(Some(BlockPointer::Finalized(block_number)))
+    }
+
+    async fn get_block(
+        &self,
+        pointer: BlockPointer,
+    ) -> DataSourceResult<Option<(Header, Vec<TxEnvelopeWithSender>)>> {
+        let block = get_block_from_archive(&self.archive_reader, pointer).await?;
+
+        Ok(block.map(|b| (b.header, b.body.transactions)))
+    }
+
+    async fn get_block_header(&self, pointer: BlockPointer) -> DataSourceResult<Option<Header>> {
+        let block = get_block_from_archive(&self.archive_reader, pointer).await?;
+
+        Ok(block.map(|b| b.header))
+    }
+}
--- monad-rpc/src/data/source/historical.rs
+++ monad-rpc/src/data/source/historical.rs
@@ -13,16 +13,43 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+use alloy_consensus::Header;
+use alloy_primitives::BlockHash;
 use async_trait::async_trait;
 use auto_impl::auto_impl;
 use dyn_clone::{clone_trait_object, DynClone};
+use monad_eth_types::TxEnvelopeWithSender;
 
-use super::DataSourceStack;
+use super::{BlockCommitState, BlockPointer, DataSourceResult, DataSourceStack};
 
 /// Trait for read-only queries against historical chain data.
 #[async_trait]
 #[auto_impl(Box)]
-pub trait HistoricalDataSource: DynClone + Send + Sync {}
+pub trait HistoricalDataSource: DynClone + Send + Sync {
+    async fn try_resolve_block_commit_state(
+        &self,
+        commit_state: BlockCommitState,
+    ) -> DataSourceResult<Option<BlockPointer>>;
+
+    async fn try_resolve_block_number(
+        &self,
+        block_number: u64,
+    ) -> DataSourceResult<Option<BlockPointer>>;
+
+    async fn try_resolve_block_hash(
+        &self,
+        block_hash: BlockHash,
+    ) -> DataSourceResult<Option<BlockPointer>>;
+
+    /// Fetch a block (header + transactions).
+    async fn get_block(
+        &self,
+        pointer: BlockPointer,
+    ) -> DataSourceResult<Option<(Header, Vec<TxEnvelopeWithSender>)>>;
+
+    /// Fetch a block header.
+    async fn get_block_header(&self, pointer: BlockPointer) -> DataSourceResult<Option<Header>>;
+}
 
 clone_trait_object!(HistoricalDataSource);
 
